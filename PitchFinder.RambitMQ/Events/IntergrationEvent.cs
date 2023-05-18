32e49428a5ddd21ae854a11e525409537e85f8b0
tronglt20 <tronglt2001@gmail.com>
2023-05-18 19:06:17 +0700

    setup Rambitmq

---
+++ PitchFinder.RambitMQ/Events/IntergrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PitchFinder.RambitMQ.Events
+{
+    public class IntergrationEvent
+    {
+        public IntergrationEvent()
+        {
+            EventId = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntergrationEvent(Guid id, DateTime createDate)
+        {
+            EventId = id;
+            CreationDate = createDate;
+        }
+
+        [JsonPropertyName("Id")]
+        public Guid EventId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ PitchFinder.RambitMQ/Handlers/IntergrantionHandlerBase.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit;
+using PitchFinder.RambitMQ.Events;
+
+namespace PitchFinder.RambitMQ.Handlers
+{
+    public abstract class IntergrantionHandlerBase<T> : IConsumer<T> where T : IntergrationEvent
+    {
+        public abstract Task Consume(ConsumeContext<T> context);
+    }
+}
